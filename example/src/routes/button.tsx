62653928c76ed52062e87874075477931840d1db
Kazuhiro Kotsutsumi <kotsutsumi@gmail.com>
2024-09-18 12:06:04 +0900

    Add preview and code switcher

--- example/src/routes/button.tsx
+++ example/src/routes/button.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 
 import { createFileRoute } from '@tanstack/react-router'
 import { Button } from '@kotsutsumi/viz-ui'
+import { Tabs } from '@ark-ui/react'
 
 export const Route = createFileRoute('/button')({
     component: PageComponent
@@ -16,12 +17,30 @@ function PageComponent() {
     return (
         <div className="prose prose-xl">
             <h1>Button</h1>
-            <div>
-                <pre className="language-javascript">
-                    <code>{`import {Button} from '@kotsutsumi/viz-ui'`}</code>
-                </pre>
+            <p>Displays a button or a component that looks like a button.</p>
+            <h2>Basic</h2>
+            <div className="code-container">
+                <Tabs.Root defaultValue="preview">
+                    <Tabs.List className="">
+                        <Tabs.Trigger value="preview" className="px-3 h-10">
+                            Preview
+                        </Tabs.Trigger>
+                        <Tabs.Trigger value="code">Code</Tabs.Trigger>
+                    </Tabs.List>
+                    <Tabs.Content value="preview">
+                        <Button />
+                    </Tabs.Content>
+                    <Tabs.Content value="code">
+                        <pre className="language-javascript">
+                            <code>{`import { Button } from '@kotsutsumi/viz-ui'
+
+export function ButtonDemo() {
+  return <Button>Button</Button>
+}`}</code>
+                        </pre>
+                    </Tabs.Content>
+                </Tabs.Root>
             </div>
-            <Button />
         </div>
     )
 }
